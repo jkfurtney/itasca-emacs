7249e16cb5f0c9254babc21df1865c4d907cc7a2
unknown <jfurtney@ABITA.pulltab.local>
2015-01-12 08:51:00 -0600

    Function to test if a given file is a FISH binary file

--- itasca.el
+++ itasca.el
@@ -996,3 +996,13 @@ Itasca code."
          (s (format template name)))
     (kill-new s)
     (message "Copied: %s to clipboard" s)))
+
+(defun itasca-fish-binary-file-p (filename)
+  (interactive)
+  "Returns true if a given file is a FISH binary file."
+  (eq 178278912
+      (cdr (assoc :fc
+                  (bindat-unpack '((:fc u32r) (:dummy u32r))
+                                 (with-temp-buffer
+                                   (insert-file-literally filename)
+                                   (string-to-unibyte (buffer-string))))))))
